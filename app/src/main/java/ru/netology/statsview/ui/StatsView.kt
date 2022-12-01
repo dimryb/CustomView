6b12533d002fd71927e952b879f87edb98e7605d
dmitry <dimryb@bk.ru>
2022-12-01 23:02:55 +0700

    Initial commit

---
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -0,0 +1,46 @@
+package ru.netology.statsview.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.View
+import ru.netology.statsview.utils.AndroidUtils
+import kotlin.math.min
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet?,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+): View(
+    context,
+    attributeSet,
+    defStyleAttr,
+    defStyleRes,
+) {
+
+    private var radius = 0F
+    private var center = PointF()
+    private val lineWidth = AndroidUtils.dp(context, 5)
+    private val paint = Paint(
+    Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = lineWidth.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+
+        radius = min(w, h) / 2F - AndroidUtils.dp(context, 5)
+        center = PointF(w / 2F, h / 2F)
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawCircle(center.x, center.y, radius, paint)
+    }
+}
