90fa60803350a98a0876a91e3a47082e2e395a2d
dmitry <dimryb@bk.ru>
2022-12-03 22:27:46 +0700

    Убрал лишний код

--- app/src/main/java/ru/netology/statsview/AppActivity.kt
+++ app/src/main/java/ru/netology/statsview/AppActivity.kt
@@ -1,16 +1,7 @@
 package ru.netology.statsview
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.view.animation.BounceInterpolator
-import android.view.animation.LinearInterpolator
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
 class AppActivity : AppCompatActivity() {
@@ -25,20 +16,5 @@ class AppActivity : AppCompatActivity() {
             500F,
             500F,
         )
-
-//        val alpha = ObjectAnimator.ofFloat(view, View.ALPHA, 0.25F, 1F).apply {
-//            duration = 300
-//            interpolator = LinearInterpolator()
-//        }
-//        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0F, 1F)
-//        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0F, 1F)
-//        val scale = ObjectAnimator.ofPropertyValuesHolder(view, scaleX, scaleY).apply {
-//            duration = 300
-//            interpolator = BounceInterpolator()
-//        }
-//        AnimatorSet().apply {
-//            startDelay = 500
-//            playSequentially(scale, alpha)
-//        }.start()
     }
 }
